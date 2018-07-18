665074d3726c5594418069bae379d08a44850194
Mikhail <tradecontent@gmail.com>
2018-07-18 17:17:59 +0800

    Added missing concerns

--- app/controllers/concerns/league_permissions.rb
+++ app/controllers/concerns/league_permissions.rb
@@ -5,6 +5,14 @@ module LeaguePermissions
     user_signed_in? && current_user.can?(:edit, :leagues)
   end
 
+  def user_can_view_leagues?
+    user_signed_in? && current_user.can?(:view, :leagues)
+  end
+  
+  def user_can_create_leagues?
+    user_signed_in? && current_user.can?(:create, :leagues)
+  end  
+  
   def user_can_edit_league?(league = nil)
     league ||= @league
 
