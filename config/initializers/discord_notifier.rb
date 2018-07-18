d4dba7b35a08777f3911a4a4942f2b2abc45098c
Mikhail <tradecontent@gmail.com>
2018-07-18 22:36:45 +0800

    Discord webhooks

--- config/initializers/discord_notifier.rb
+++ config/initializers/discord_notifier.rb
@@ -1,7 +1,7 @@
 require 'discord_notifier'
 
 Discord::Notifier.setup do |config|
-  config.url = 'https://discordapp.com/api/webhooks/431476194492416001/CRlaBPavgeBVducLdFYlcBFVzNZeR5qNZywXL_qS_PvRU_3sFnW7thZ2PYSQ4jagI2lz'
+  config.url = 'https://discordapp.com/api/webhooks/423731348562509825/2_zLy56VY2NuSEC330t6frogGZKXC1H-d8dlen_ztbuO9db-p5-T4pSIbcU9PyboUL1R'
 
   # Defaults to `false`
   config.wait = true
