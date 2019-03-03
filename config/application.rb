d236fe25a7480748c2573b4c732f28c19969378a
Mikhail Korolev <self@mkrl.xyz>
2019-03-03 19:22:16 +0800

    Upgraded to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -8,22 +8,18 @@ Bundler.require(*Rails.groups)
 
 module Ozfortress
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     Raven.configure do |config|
       config.dsn = Rails.application.secrets.sentry_dsn 
-    end    
-    
+    end
+
     # Make view helpers, view specific
     config.action_controller.include_all_helpers = false
 
