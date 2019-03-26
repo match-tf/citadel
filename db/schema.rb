1e1e92e324921afe61092d227d6e7c2738cc4117
Mikhail Korolev <self@mkrl.xyz>
2019-03-26 16:01:49 +0800

    Fix roster changes not triggering score/placement updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_230242) do
+ActiveRecord::Schema.define(version: 2019_02_01_085058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
